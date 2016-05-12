34fc4efa285c8ec3a3a4528d64b6880b5b336942
Jeff Rauch <hello@jeffrau.ch>
2016-05-12 11:54:39 -0700

    Add GitIgnore File

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var twoPeople: UILabel!
+    @IBOutlet weak var threePeople: UILabel!
+    @IBOutlet weak var fourPeople: UILabel!
+    @IBOutlet weak var blueBg: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        billField.becomeFirstResponder()
+        
+        billField.text = ""
+        tipLabel.text = "$0.00"
+        
+        // Movement
+        self.billField.transform = CGAffineTransformMakeTranslation(0,100)
+        self.billLabel.transform = CGAffineTransformMakeTranslation(0,100)
+        self.blueBg.transform = CGAffineTransformMakeTranslation(0,1000)
+        self.totalLabel.transform = CGAffineTransformMakeTranslation(0,1000)
+        self.tipControl.transform = CGAffineTransformMakeTranslation(0,1000)
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,56 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        if billField.text == "" {
+            
+            UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 2, initialSpringVelocity: 0.05, options: [], animations: {
+            
+                // Movement
+                self.billField.transform = CGAffineTransformMakeTranslation(0,100)
+                self.billLabel.transform = CGAffineTransformMakeTranslation(0,100)
+                self.blueBg.transform = CGAffineTransformMakeTranslation(0,1000)
+                self.totalLabel.transform = CGAffineTransformMakeTranslation(0,1000)
+                self.tipControl.transform = CGAffineTransformMakeTranslation(0,1000)
+                
+            } , completion: nil)
+        
+        }
+        
+        else {
+        
+        UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 2, initialSpringVelocity: 0.05, options: [], animations: {
+            
+            // Movement
+            self.billField.transform = CGAffineTransformMakeTranslation(0,0)
+            self.billLabel.transform = CGAffineTransformMakeTranslation(0,0)
+            self.blueBg.transform = CGAffineTransformMakeTranslation(0,0)
+            self.totalLabel.transform = CGAffineTransformMakeTranslation(0,0)
+            self.tipControl.transform = CGAffineTransformMakeTranslation(0,0)
+            
+            } , completion: nil)
+            
+        }
+        
+        var tipPercentages = [0.18, 0.20, 0.22]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        twoPeople.text = String(format: "$%.2f", total / 2)
+        threePeople.text = String(format: "$%.2f", total / 3)
+        fourPeople.text = String(format: "$%.2f", total / 4)
+        
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+        
+    }
 }
 
